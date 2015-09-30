a10396433030a80342605cff171c25830c4763c0
Renan Aragão <renanferreiradearagao@yahoo.com.br>
2015-09-30 17:01:31 -0300

    Alterado o arquivo test/PromiseTestSpec.js

--- test/PromiseTestSpec.js
+++ test/PromiseTestSpec.js
@@ -1,9 +1,8 @@
+describe('Promise - ', function(){
 'use strict';
 
-describe('Promise - ', function(){
-
-	var scope, 
-        facPessoa, 
+	var scope,
+        facPessoa,
         server,
         httpBackend;
 
@@ -18,61 +17,64 @@ describe('Promise - ', function(){
 	});
 
 	beforeEach(inject(function($rootScope, $httpBackend) {
-    	scope = $rootScope.$new();  
-        
+    	scope = $rootScope.$new();
+
         httpBackend = $httpBackend;
-        
+
         httpBackend.when('GET', '/pessoa').respond({nome: "Renan", idade: 23});
-        
+
         server = sinon.fakeServer.create();
-        
+
         server.respondWith('GET', '/pessoa', JSON.stringify({nome: "Renan", idade: 23}));
-        
-        
+
+
 	}));
-    
+
     afterEach(function(){
-        
+
         server.restore();
-        
+
         httpBackend.verifyNoOutstandingExpectation();
         httpBackend.verifyNoOutstandingRequest();
-    
+
     });
 
 	it('Deve retornar objeto pessoa com nome "Renan" e idade "23" - Com XMLHttpRequest', function(){
 
         var result;
-        
-		myApp.getPessoa().then(function(data){
-        
+
+				myApp.getPessoa().then(function(data){
+
             result = data;
-            
+
         });
 
         server.respond();
-        
+
         var pessoa = JSON.parse(result.target.response);
-        
+
         expect(result.target.method).toEqual('GET');
         expect(result.target.url).toEqual('/pessoa');
-        expect(pessoa.nome).toEqual('Renan');
         
+        expect(pessoa.nome).toEqual('Renan');
+        expect(pessoa.idade).toEqual(23);
+
 	});
 
 	it('Deve retornar objeto pessoa da fábrica com nome "Renan" e idade "23" - Angular Mocks', function(){
 
-		var pessoa;
-        
+				var pessoa;
+
         var spy = jasmine.createSpy('spy');
 
-		facPessoa.getPessoa().then(function(data){
+				facPessoa.getPessoa().then(function(data){
             pessoa = data;
         });
 
         httpBackend.flush();
-        
+
         expect(pessoa.nome).toEqual('Renan');
+        expect(pessoa.idade).toEqual(23);
 
 	});
 
